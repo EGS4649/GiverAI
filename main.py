13bc8a3ca446077380159c7cb06a48e6d93710b8
AE <alex.ellis@mail.utoronto.ca>
2026-02-10 18:24:26 -0500

    dashboard fix

--- main.py
+++ main.py
@@ -2962,6 +2962,7 @@ async def register_post(
             "recaptcha_site_key": os.getenv("RECAPTCHA_SITE_KEY"),
             "csrf_token": new_csrf_token
         })
+        response = RedirectResponse(url="/dashboard", status_code=303)
         response.set_cookie(
             key="fastapi-csrf-token",
             value=new_csrf_token,
